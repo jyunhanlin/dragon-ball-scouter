77998c2e1a2a8a2873574d339778d0b571ccbc39
Hank Lin <jyunhanlin@gmail.com>
2026-07-21 22:40:29 +0800

    ci: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  base: './',
   plugins: [basicSsl()],
   server: { host: true },
   test: { environment: 'node' },
